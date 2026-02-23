9f597e9a40364c700dca8d2df02d996cc968c3a4
RiveraXimena <riverahernandezximena122@gmail.com>
2026-02-22 22:24:48 -0600

    Agrupacion fisica y logica

---
+++ AssemblyNamespace/AssemblyNamespace/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyNamespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader myStreamRead =
+                new System.IO.StreamReader("Hola.txt");
+
+            string line = myStreamRead.ReadLine();
+
+            Console.WriteLine(line);
+
+            myStreamRead.Close();
+            Console.ReadLine();
+        }
+    }
+}
