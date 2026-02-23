f36fc01a965d53f95aa40220344104f0a061ae28
RiveraXimena <riverahernandezximena122@gmail.com>
2026-02-22 22:06:48 -0600

    Expresiones

---
+++ OperatorsExpressionsStatements/OperatorsExpressionsStatements/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsExpressionsStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, a, b;
+
+            x = 1;
+            y = 8;
+            a = 2;
+            b = 4;
+
+            x = 1 + 8;
+            x = 4 - 2;
+            x = 8 * 4;
+
+            if (x == y)
+            {
+            }
+            if (x > y)
+            {
+            }
+            if (x < y)
+            {
+            }
+            if (x >= y)
+            {
+            }
+            if (x <= y)
+            {
+            }
+
+            if ((x > y) && (a > b)) 
+            { 
+            }
+
+            if ((x > y) || (a > b))
+            {
+            }
+
+            string message = (x == 1) ? "Paleta" : "Dulce";
+
+            Console.WriteLine("Hola");
+        }
+    }
+}
