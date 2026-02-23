d7747357e888f0a14af810ec36eaf6f6517a0cdd
RiveraXimena <riverahernandezximena122@gmail.com>
2026-02-22 22:31:38 -0600

    Switch

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe el nombre de una mascota para ver su sonido:");
+            string userValue = Console.ReadLine();
+            switch (userValue.ToUpper())
+            {
+                case "PERRO":
+                    Console.WriteLine("Ladra");
+                    break;
+                case "GATO":
+                    Console.WriteLine("Maulla");
+                    break;
+                case "LORO":
+                    Console.WriteLine("Imita sonidos");
+                    break;
+                default:
+                    Console.WriteLine("Does not compute");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
