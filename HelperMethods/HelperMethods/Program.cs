f0769b984c701b50be94bac5f6de8c405d6a14af
RiveraXimena <riverahernandezximena122@gmail.com>
2026-02-22 22:16:22 -0600

    HelperMethods

---
+++ HelperMethods/HelperMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string Valor = Formula("Mundo");
+            string Valor = Formula("Ximena");
+            Console.WriteLine(Valor);
+            Console.ReadLine();
+        }
+
+        private static string Formula()
+        {
+            return "Hola mundo!";
+        }
+
+        private static string Formula(string nombre)
+        {
+            return String.Format("Hola, {0}!", nombre);
+        }
+    }
+}
