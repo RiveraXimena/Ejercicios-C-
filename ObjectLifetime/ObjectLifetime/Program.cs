d609d99e5f88fcad63c4c7964817464b93f84222
RiveraXimena <riverahernandezximena122@gmail.com>
2026-02-22 22:21:48 -0600

    Periodos de creacion

---
+++ ObjectLifetime/ObjectLifetime/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifetime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro myCar = new Carro();
+
+            Carro Carro2 = myCar;
+
+            Carro Carro3 = new Carro("Nissan", "Coche", 2006, "Blanco");
+
+            Carro2 = null;
+            myCar = null;
+
+            Carro.MyMethod();
+        }
+    }
+
+    class Carro 
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public string Color { get; set; }
+        public double PrecioOriginal { get; set; }
+
+        public Carro() 
+        {
+            this.Marca = "Nissan";
+        }
+
+        public Carro(string marca, string modelo, int año, string color)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Año = año;
+            Color = color;
+        }
+
+        /*public Carro(string Otro, string modelo, int año, string color)
+        {
+            Marca = Otro;
+            Modelo = modelo;
+            Año = año;
+            Color = color;
+        }
+        */
+        public static void MyMethod()
+        {
+            
+        }
+    }
+}
