15aa5153b3e4bbdf465ff685e7e380bc3bea2d89
RiveraXimena <riverahernandezximena122@gmail.com>
2026-02-22 22:33:02 -0600

    Statement

---
+++ Statement/Statement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selecciona un numero entre el 1 y 100");
+            string userValue = Console.ReadLine();
+            
+            int compareValue = int.Parse(userValue);
+            
+            if ((compareValue < 1) || (compareValue > 100))
+                Console.WriteLine("El numero que escribiste esta fuera del rango");
+            else if ((compareValue == 42) || (compareValue > 90))
+                Console.WriteLine("Encontraste el numero especial");
+            else
+                Console.WriteLine("No encontraste numeros especiales");
+            Console.ReadLine();
+        }
+    }
+}
