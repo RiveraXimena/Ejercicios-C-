7500a3aa7e325a74f03d62b939a9ff48c219ea91
RiveraXimena <riverahernandezximena122@gmail.com>
2026-02-22 22:36:06 -0600

    Enumeracion

---
+++ Enumerations/Enumerations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            //Console.WriteLine("Hello world!");
+            Console.WriteLine("Escribe el nombre de una mascota y te dare su sonido ");
+            string userValue = Console.ReadLine();
+
+            Mascota myValue;
+
+            if (Enum.TryParse<Mascota>(userValue, true, out myValue))
+            {
+
+                switch (myValue)
+                {
+                    case Mascota.Perro:
+                        Console.WriteLine("Ladra");
+                        break;
+                    case Mascota.Gato:
+                        Console.WriteLine("Maulla");
+                        break;
+                    case Mascota.Loro:
+                        Console.WriteLine("Imita sonidos");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ese nobre no se encuentra en el programa");
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+
+    enum Mascota
+    {
+        Perro,
+        Gato, Loro
+    }
+
+}
