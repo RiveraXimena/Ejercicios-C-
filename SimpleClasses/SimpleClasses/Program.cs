a81b38c9c924e7dbe309a681859b701ad093e777
RiveraXimena <riverahernandezximena122@gmail.com>
2026-02-22 22:19:23 -0600

    Clases simples

---
+++ SimpleClasses/SimpleClasses/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro CarroNuevo = new Carro();
+
+            CarroNuevo.Marca = "Nissan";
+            CarroNuevo.Modelo = "Coche";
+            CarroNuevo.Año = 2006;
+            CarroNuevo.Color = "Blanco";
+
+            Console.WriteLine("{0} - {1} - {2}", CarroNuevo.Marca, CarroNuevo.Modelo, CarroNuevo.Color);
+
+            //double ValorOficial = determinarValorOficial(CarroNuevo);
+
+            Console.WriteLine("Valor del carro: {0:c}", CarroNuevo.determinarValorOficial());
+            Console.ReadLine();
+        }
+
+        private static double determinarValorOficial(Carro _carro)
+        {
+            double carroValor = 100.0;
+            return carroValor;
+        }
+    }
+
+    class Carro 
+    { 
+        public string Marca {  get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public string Color { get; set; }
+
+        public double determinarValorOficial()
+        {
+            double carroValor = 100.0;
+            if (this.Año > 1990)
+                carroValor = 10000.0;
+            else
+                carroValor = 2000.0;
+            return carroValor;
+        }
+
+    }
+}
---
+++ SimpleClasses/SimpleClasses/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("SimpleClasses")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleClasses")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("62a7378d-86df-4e22-b785-9c0b84487da1")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
